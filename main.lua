8f0d781ceee071f938a91cbdd070d35a0a9499cc
MikaMeijer <dev@mikameijer.dev>
2023-04-03 11:05:55 +0200

    initial commit

---
+++ main.lua
@@ -0,0 +1,5 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Sabotax", "Midnight")
+
+local Tab = Window:NewTab("Test")
+local Section = Tab:NewSection("Test section")
